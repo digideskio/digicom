64043ee362854c862df80f8a629f880bc9bf34cf
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-17 05:07:00 +0600

    robofile, codeception n test from weblink added

---
+++ tests/acceptance/01-InstallWeblinksCest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_weblinks
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use \AcceptanceTester;
+
+class InstallWeblinksCest
+{
+	public function installJoomla(AcceptanceTester $I)
+	{
+		$I->am('Administrator');
+		$I->installJoomla();
+		$I->doAdministratorLogin();
+		$I->setErrorReportingToDevelopment();
+	}
+
+	// tests
+	public function installWeblinks(AcceptanceTester $I)
+	{
+		$I->doAdministratorLogin();
+		$I->comment('get Weblinks repository folder from acceptance.suite.yml (see _support/AcceptanceHelper.php)');
+		$path = $I->getConfiguration('repo_folder');
+		$I->installExtensionFromFolder($path . 'src/com_weblinks/');
+		$I->doAdministratorLogout();
+	}
+}
