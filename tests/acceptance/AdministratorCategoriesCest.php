53fd69b237adbbea46ba712a541f800e8fac776b
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2015-09-17 14:16:38 +0600

    simple test

--- tests/acceptance/AdministratorCategoriesCest.php
+++ tests/acceptance/AdministratorCategoriesCest.php
@@ -19,7 +19,7 @@ class AdministratorCategoriesCest
         $I->doAdministratorLogin();
 
         $I->amGoingTo('Navigate to Categories page in /administrator/');
-        $I->amOnPage('administrator/index.php?option=com_digicom&view=categories');
+        $I->amOnPage('/administrator/index.php?option=com_digicom&view=categories&extension=com_digicom');
         $I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
         $I->expectTo('see categories page');
         $I->checkForPhpNoticesOrWarnings();
@@ -41,7 +41,7 @@ class AdministratorCategoriesCest
         $I->doAdministratorLogin();
 
         $I->amGoingTo('Navigate to Categories page in /administrator/');
-        $I->amOnPage('administrator/index.php?option=com_digicom&view=categories');
+        $I->amOnPage('/administrator/index.php?option=com_digicom&view=categories&extension=com_digicom');
         $I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
         $I->expectTo('see categories page');
 
@@ -52,87 +52,4 @@ class AdministratorCategoriesCest
         $I->expectTo('see an error when trying to save a category without title');
         $I->see('Invalid field:  Title',['id' => 'system-message-container']);
     }
-
-  	public function administratorPublishDigicom(AcceptanceTester $I)
-  	{
-  		$I->am('Administrator');
-  		$I->wantToTest('Category publishing in /administrator/');
-
-  		$I->doAdministratorLogin();
-
-  		$I->amGoingTo('Navigate to Categories page in /administrator/');
-  		$I->amOnPage('administrator/index.php?option=com_digicom&view=categories');
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->expectTo('see categories page');
-  		$I->checkForPhpNoticesOrWarnings();
-
-  		$I->amGoingTo('try to save a category with a filled title');
-  		$I->click(['xpath'=> "//button[@onclick=\"Joomla.submitbutton('category.add')\"]"]);
-  		$I->waitForText('Add Category','30',['css' => 'h1']);
-  		$I->fillField(['id' => 'jform_title'],'automated testing pub' . rand(1,100));
-  		$I->click(['xpath'=> "//button[@onclick=\"Joomla.submitbutton('category.save')\"]"]);
-
-  		$I->expectTo('see a success message after saving the category');
-  		$I->see('Item successfully saved',['id' => 'system-message-container']);
-
-  		$I->amGoingTo('Search for automated testing');
-  		$I->fillField(['xpath'=> "//input[@id=\"filter_search\"]"], "automated testing pub" . "\n");
-
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->amGoingTo('Select the first category');
-  		$I->click(['xpath'=> "//input[@id=\"cb0\"]"]);
-
-  		$I->amGoingTo('try to publish a digicom category');
-  		$I->click(['xpath'=> "//button[@onclick=\"if (document.adminForm.boxchecked.value==0){alert('Please first make a selection from the list.');}else{ Joomla.submitbutton('categories.publish')}\"]"]);
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->expectTo('see a success message after publishing the category');
-  		$I->see('1 item successfully published.',['id' => 'system-message-container']);
-  	}
-
-  	public function administratorUnpublishWeblink(AcceptanceTester $I)
-  	{
-  		$I->am('Administrator');
-  		$I->wantToTest('Category unpublishing in /administrator/');
-
-  		$I->doAdministratorLogin();
-
-  		$I->amGoingTo('Navigate to Categories page in /administrator/');
-  		$I->amOnPage('administrator/index.php?option=com_digicom&view=categories');
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->expectTo('see categories page');
-  		$I->checkForPhpNoticesOrWarnings();
-
-  		$I->amGoingTo('try to save a category with a filled title');
-  		$I->click(['xpath'=> "//button[@onclick=\"Joomla.submitbutton('category.add')\"]"]);
-  		$I->waitForText('Add Category','30',['css' => 'h1']);
-  		$I->fillField(['id' => 'jform_title'],'automated testing unpub' . rand(1,100));
-  		$I->click(['xpath'=> "//button[@onclick=\"Joomla.submitbutton('category.save')\"]"]);
-
-  		$I->expectTo('see a success message after saving the category');
-  		$I->see('Item successfully saved',['id' => 'system-message-container']);
-
-  		$I->amGoingTo('Search for automated testing');
-  		$I->fillField(['xpath'=> "//input[@id=\"filter_search\"]"], "automated testing unpub" . "\n");
-
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->amGoingTo('Select the first category');
-  		$I->click(['xpath'=> "//input[@id=\"cb0\"]"]);
-
-  		$I->amGoingTo('Try to publish a digicom category');
-  		$I->click(['xpath'=> "//button[@onclick=\"if (document.adminForm.boxchecked.value==0){alert('Please first make a selection from the list.');}else{ Joomla.submitbutton('categories.publish')}\"]"]);
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->expectTo('See a success message after publishing the category');
-  		$I->see('1 item successfully published.',['id' => 'system-message-container']);
-
-  		// Unpublish it again
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->amGoingTo('Select the first category');
-  		$I->click(['xpath'=> "//input[@id=\"cb0\"]"]);
-
-  		$I->amGoingTo('Try to unpublish a digicom category');
-  		$I->click(['xpath'=> "//button[@onclick=\"if (document.adminForm.boxchecked.value==0){alert('Please first make a selection from the list.');}else{ Joomla.submitbutton('categories.unpublish')}\"]"]);
-  		$I->waitForText('Digicom Categories Manager','30',['css' => 'h1']);
-  		$I->expectTo('See a success message after unpublishing the category');
-  		$I->see('1 item successfully unpublished',['id' => 'system-message-container']);
-  	}
 }
